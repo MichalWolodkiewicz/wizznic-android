6f1019bc9a068d88886ff8458c0d63e5c22d03fd
Michal <office.mikrosoft@gmail.com>
2016-05-22 11:49:53 +0200

    Base project with SDL2. Simple image is displayed on device's screen.

---
+++ jni/Application.mk
@@ -0,0 +1,7 @@
+
+# Uncomment this if you're using STL in your project
+# See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
+APP_STL := stlport_static 
+#armeabi armeabi-v7a 
+
+APP_ABI := x86
